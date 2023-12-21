dbe028ac4bab6863dd326535559e23505ab0e9b5
nori07 <suratonline95@gmail.com>
2023-12-22 02:10:15 +0700

    menambhakan halaman beranda dan menambah style pada halaman login

--- system/view/Home.php
+++ system/view/Home.php
@@ -1,9 +1,26 @@
-<h1>
-  Page Home
-</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="/public/img/library.png" type="image/x-icon">
+  <title>Wellcome</title>
+  <link rel="stylesheet" href="/public/css/style.css">
+  <link rel="stylesheet" href="/public/css/home.css">
+</head>
+<body>
+  
+<div class="container-fluid home">
+  <div class="home-items">
+    <a href="/login">Login</a> or 
+    <a href="/register">Register</a>
+  </div>
+  <div class="footer">
+    <p>
+      Copyright 2023 &copy; Powered by Kurnia Nur Hikmah | Nori Nofandi | Ulinuha Maulaya.
+    </p>
+  </div>
+</div>
 
-<hr>
-
-<p>
-  <a href="/login">login</a>
-</p>
+</body>
+</html>
--- system/view/Login.php
+++ system/view/Login.php
@@ -5,35 +5,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Login</title>
+  <link rel="stylesheet" href="/public/css/style.css">
+  <link rel="stylesheet" href="/public/css/login.css">
 </head>
 <body>
 
-<div class="container">
+<div class="container-fluid login">
+  <div class="form">
+    <?php
+    session_start();
+    if (isset($_SESSION['error_message'])) {
+      echo "<div style='padding: 1em; margin: 1em 0; background-color: red; border-radius: 8px; color: white;'>" . $_SESSION['error_message'] . "</div>";
+      unset($_SESSION['error_message']); // Hapus pesan kesalahan setelah ditampilkan
+    }
+    ?>
 
-  <a href="/">Home</a>
-  <hr>
-
-  <?php
-  session_start();
-  if (isset($_SESSION['error_message'])) {
-    echo "<div style='padding: 1em; margin: 1em 0; background-color: red; border-radius: 8px; color: white;'>" . $_SESSION['error_message'] . "</div>";
-    unset($_SESSION['error_message']); // Hapus pesan kesalahan setelah ditampilkan
-  }
-  ?>
-
-  <form action="auth" method="post">
-    <div class="form-item">
-      <label for="email">Email</label>
-      <input type="text" id="email" name="email">
-    </div>
-    <div class="form-item">
-      <label for="password">Password</label>
-      <input type="password" id="password" name="password">
-    </div>
-    <div class="form-item">
-      <button>Login</button>
-    </div>
-  </form>
+    <form action="auth" method="post">
+      <div class="form-item">
+        <label class="label" for="email">Email</label>
+        <input type="text" id="email" name="email">
+      </div>
+      <div class="form-item">
+        <label class="label" for="password">Password</label>
+        <input type="password" id="password" name="password">
+      </div>
+      <div class="form-item">
+        <button>Login</button>
+        <a href="/">Home</a>
+      </div>
+    </form>
+  </div>
 </div>
 
 </body>
